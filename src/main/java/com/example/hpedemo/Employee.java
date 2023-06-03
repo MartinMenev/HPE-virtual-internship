fa8387afbd3bdab009e5bbd1075db1686d7fe1ce
Martin Menev <martin.menew@gmail.com>
2023-06-03 14:56:34 +0300

    Added POST option

--- src/main/java/com/example/hpedemo/Employee.java
+++ src/main/java/com/example/hpedemo/Employee.java
@@ -1,11 +1,11 @@
 package com.example.hpedemo;
 
 public class Employee {
-    private final String employee_id;
-    private final String first_name;
-    private final String last_name;
-    private final String email;
-    private final String title;
+    private String employee_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String title;
 
     public Employee(String employee_id, String first_name, String last_name, String email, String title) {
         this.employee_id = employee_id;
@@ -33,5 +33,30 @@ public class Employee {
 
     public String getTitle() {
         return title;
+    }
+
+    public Employee setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+        return this;
+    }
+
+    public Employee setFirst_name(String first_name) {
+        this.first_name = first_name;
+        return this;
+    }
+
+    public Employee setLast_name(String last_name) {
+        this.last_name = last_name;
+        return this;
+    }
+
+    public Employee setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Employee setTitle(String title) {
+        this.title = title;
+        return this;
     }
 }
--- src/main/java/com/example/hpedemo/EmployeeController.java
+++ src/main/java/com/example/hpedemo/EmployeeController.java
@@ -1,11 +1,17 @@
 package com.example.hpedemo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
+
     private final EmployeeManager employeeManager;
 
     @Autowired
@@ -14,10 +20,34 @@ public class EmployeeController {
     }
 
     @GetMapping(
-            path = "/employees",
+            path = "/",
             produces = "application/json")
     public Employees getEmployees() {
         return employeeManager.getAllEmployees();
+    }
+
+    @PostMapping(
+            path = "/",
+            consumes = "application/json",
+            produces = "application/json")
+
+    public ResponseEntity<Object> addEmployee(
+            @RequestBody Employee employee) {
+
+        employeeManager
+                .addEmployee(employee);
+
+        URI location
+                = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(
+                        employee.getEmployee_id())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .build();
     }
 
 }
